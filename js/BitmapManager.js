7f01b32e0a24b8e1c9985b2420fdb3fbf0783ed3
just bob <rsmcclure75@gmail.com>
2026-08-27 13:57:35 -0400

    Add BitmapManager and BloxField

---
+++ js/BitmapManager.js
@@ -0,0 +1,146 @@
+class BitmapManager {
+  static instance = null;
+
+  constructor() {
+    this.cache = new Map();
+  }
+
+  static initialize() {
+    BitmapManager.instance = new BitmapManager();
+    return BitmapManager.instance;
+  }
+
+  static getInstance() {
+    if (!BitmapManager.instance) BitmapManager.initialize();
+    return BitmapManager.instance;
+  }
+
+  get(name) {
+    return this.cache.get(name) || null;
+  }
+
+  getBitmapforResource(name) {
+    return this.get(name);
+  }
+
+  makeTile(name, color, label) {
+    const c = document.createElement('canvas');
+    c.width = 40;
+    c.height = 40;
+    const ctx = c.getContext('2d');
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.roundRect(2, 2, 36, 36, 6);
+    ctx.fill();
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+    ctx.stroke();
+    if (label) {
+      ctx.fillStyle = '#111';
+      ctx.font = 'bold 13px sans-serif';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(label, 20, 21);
+    }
+    this.cache.set(name, c);
+    return c;
+  }
+
+  makeBoard(w, h) {
+    const c = document.createElement('canvas');
+    c.width = w;
+    c.height = h;
+    const ctx = c.getContext('2d');
+    ctx.fillStyle = '#1a2430';
+    ctx.fillRect(0, 0, w, h);
+    ctx.strokeStyle = 'rgba(255,255,255,0.06)';
+    const cell = 40;
+    for (let x = 40; x < w; x += cell) {
+      ctx.beginPath();
+      ctx.moveTo(x + 0.5, 40);
+      ctx.lineTo(x + 0.5, h - 40);
+      ctx.stroke();
+    }
+    for (let y = 40; y < h; y += cell) {
+      ctx.beginPath();
+      ctx.moveTo(40, y + 0.5);
+      ctx.lineTo(w - 40, y + 0.5);
+      ctx.stroke();
+    }
+    ctx.strokeStyle = '#3a4a5a';
+    ctx.lineWidth = 4;
+    ctx.strokeRect(38, 38, w - 76, h - 76);
+    this.cache.set('whole_bg_ls', c);
+    return c;
+  }
+
+  loadPlaceholders() {
+    this.makeBoard(480, 320);
+    this.makeTile('blx_wall', '#5a5a5a', '');
+    this.makeTile('blx_generic_movable', '#c8c8d0', '');
+    this.makeTile('blx_o', '#3cb4ff', 'O');
+    this.makeTile('blx_x', '#ff5a5a', 'X');
+    this.makeTile('blx_hole', '#0a0a0a', '\u25cf');
+    this.makeTile('blx_bomb', '#e0a020', 'B');
+    this.makeTile('blx_bomb_inactive', '#806010', 'b');
+    this.makeTile('blx_breakable', '#8a6a4a', '/');
+    this.makeTile('blx_bolts', '#8899aa', '+');
+    this.makeTile('blx_bridge_pot', '#6a8a4a', 'P');
+    this.makeTile('blx_bridge', '#4a7a3a', '=');
+    this.makeTile('blx_oneway_left_fg', '#4a6aaa', '\u2190');
+    this.makeTile('blx_oneway_left_bg', '#2a3a55', '');
+    this.makeTile('blx_oneway_right_fg', '#4a6aaa', '\u2192');
+    this.makeTile('blx_oneway_right_bg', '#2a3a55', '');
+    this.makeTile('blx_oneway_up_fg', '#4a6aaa', '\u2191');
+    this.makeTile('blx_oneway_up_bg', '#2a3a55', '');
+    this.makeTile('blx_oneway_down_fg', '#4a6aaa', '\u2193');
+    this.makeTile('blx_oneway_down_bg', '#2a3a55', '');
+    this.makeTile('align_target_bg', '#224466', '');
+    this.makeTile('align_target_arrow', '#88ccff', '+');
+    this.makeTile('effect_cloud', '#dddddd', '');
+    this.makeTile('effect_explosion', '#ff8844', '*');
+    return this;
+  }
+
+  async loadAll() {
+    this.loadPlaceholders();
+    const files = {
+      whole_bg_ls: 'assets/images/whole_bg_ls.png',
+      blx_wall: 'assets/images/blx_wall.png',
+      blx_generic_movable: 'assets/images/blx_generic_movable.png',
+      blx_o: 'assets/images/blx_o.png',
+      blx_x: 'assets/images/blx_x.png',
+      blx_hole: 'assets/images/blx_hole.png',
+      blx_bomb: 'assets/images/blx_bomb.png',
+      blx_bomb_inactive: 'assets/images/blx_bomb_inactive.png',
+      blx_breakable: 'assets/images/blx_breakable.png',
+      blx_bolts: 'assets/images/blx_bolts.png',
+      blx_bridge_pot: 'assets/images/blx_bridge_pot.png',
+      blx_bridge: 'assets/images/blx_bridge.png',
+      blx_oneway_left_fg: 'assets/images/blx_oneway_left_fg.png',
+      blx_oneway_left_bg: 'assets/images/blx_oneway_left_bg.png',
+      blx_oneway_right_fg: 'assets/images/blx_oneway_right_fg.png',
+      blx_oneway_right_bg: 'assets/images/blx_oneway_right_bg.png',
+      blx_oneway_up_fg: 'assets/images/blx_oneway_up_fg.png',
+      blx_oneway_up_bg: 'assets/images/blx_oneway_up_bg.png',
+      blx_oneway_down_fg: 'assets/images/blx_oneway_down_fg.png',
+      blx_oneway_down_bg: 'assets/images/blx_oneway_down_bg.png',
+      align_target_bg: 'assets/images/align_target_bg.png',
+      align_target_arrow: 'assets/images/align_target_arrow.png',
+      effect_cloud: 'assets/images/effect_cloud.png',
+      effect_explosion: 'assets/images/effect_explosion.png'
+    };
+    const loads = Object.entries(files).map(([name, url]) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.onload = () => {
+          this.cache.set(name, img);
+          resolve();
+        };
+        img.onerror = () => resolve();
+        img.src = url;
+      });
+    });
+    await Promise.all(loads);
+    return this;
+  }
+}
---
+++ js/BloxField.js
@@ -0,0 +1,124 @@
+class BloxField {
+  constructor(game) {
+    this.g = game;
+    this.field = [];
+    this.underfield = [];
+    for (let x = 0; x < game.BLoxCntX; x++) {
+      this.field[x] = [];
+      this.underfield[x] = [];
+      for (let y = 0; y < game.BLoxCntY; y++) {
+        this.field[x][y] = [];
+        this.underfield[x][y] = [];
+      }
+    }
+  }
+
+  clearAll() {
+    const g = this.g;
+    for (let x = 0; x < g.BLoxCntX; x++) {
+      for (let y = 0; y < g.BLoxCntY; y++) {
+        this.field[x][y].length = 0;
+        this.underfield[x][y].length = 0;
+      }
+    }
+  }
+
+  addBloxToField(aBlox, field) {
+    if (field.indexOf(aBlox) < 0) field.push(aBlox);
+    if (aBlox.myFieldLocs.indexOf(field) < 0) aBlox.myFieldLocs.push(field);
+  }
+
+  removeBloxFromTheWorld(aBlox) {
+    const g = this.g;
+    BloxField._removeFrom(g.underBloxs, aBlox);
+    BloxField._removeFrom(g.surfaceBloxs, aBlox);
+    BloxField._removeFrom(g.overBloxs, aBlox);
+    BloxField._removeFrom(g.moverBloxs, aBlox);
+
+    if (aBlox === g.bloxO1) g.bloxO1 = null;
+    else if (aBlox === g.bloxX) g.bloxX = null;
+    else if (aBlox === g.bloxO2) g.bloxO2 = null;
+
+    for (let i = 0; i < aBlox.myFieldLocs.length; i++) {
+      BloxField._removeFrom(aBlox.myFieldLocs[i], aBlox);
+    }
+
+    if (aBlox.bolted != null) {
+      BloxField._removeFrom(g.overBloxs, aBlox.bolted);
+      aBlox.bolted = null;
+    }
+  }
+
+  static _removeFrom(arr, item) {
+    const i = arr.indexOf(item);
+    if (i >= 0) arr.splice(i, 1);
+  }
+
+  giveMeListOfFieldPointsFor(x, y, list) {
+    const g = this.g;
+    list.length = 0;
+    const fieldlocx = Math.trunc((x - g.LEFT) / g.bloxDist);
+    const fieldlocy = Math.trunc((y - g.TOP) / g.bloxDist);
+    const fieldlocxOff = (x - g.LEFT) % g.bloxDist;
+    const fieldlocyOff = (y - g.TOP) % g.bloxDist;
+    if (!this._valid(fieldlocx, fieldlocy)) return;
+    list.push(this.field[fieldlocx][fieldlocy]);
+    if (fieldlocxOff > 0 && fieldlocx + 1 < g.BLoxCntX && this._valid(fieldlocx + 1, fieldlocy)) {
+      list.push(this.field[fieldlocx + 1][fieldlocy]);
+    }
+    if (fieldlocyOff > 0 && fieldlocy + 1 < g.BLoxCntY && this._valid(fieldlocx, fieldlocy + 1)) {
+      list.push(this.field[fieldlocx][fieldlocy + 1]);
+    }
+    if (fieldlocxOff > 0 && fieldlocx + 1 < g.BLoxCntX && fieldlocyOff > 0 && fieldlocy + 1 < g.BLoxCntY) {
+      list.push(this.field[fieldlocx + 1][fieldlocy + 1]);
+    }
+  }
+
+  giveMeListOfBloxInTheFieldPointsFor(x, y, list) {
+    const g = this.g;
+    list.length = 0;
+    const fieldlocx = Math.trunc((x - g.LEFT) / g.bloxDist);
+    const fieldlocy = Math.trunc((y - g.TOP) / g.bloxDist);
+    const fieldlocxOff = (x - g.LEFT) % g.bloxDist;
+    const fieldlocyOff = (y - g.TOP) % g.bloxDist;
+    if (!this._valid(fieldlocx, fieldlocy)) return;
+    this._addAll(list, this.field[fieldlocx][fieldlocy]);
+    if (fieldlocxOff > 0 && fieldlocx + 1 < g.BLoxCntX && this._valid(fieldlocx + 1, fieldlocy)) {
+      this._addAll(list, this.field[fieldlocx + 1][fieldlocy]);
+    }
+    if (fieldlocyOff > 0 && fieldlocy + 1 < g.BLoxCntY && this._valid(fieldlocx, fieldlocy + 1)) {
+      this._addAll(list, this.field[fieldlocx][fieldlocy + 1]);
+    }
+    if (fieldlocxOff > 0 && fieldlocx + 1 < g.BLoxCntX && fieldlocyOff > 0 && fieldlocy + 1 < g.BLoxCntY) {
+      this._addAll(list, this.field[fieldlocx + 1][fieldlocy + 1]);
+    }
+  }
+
+  giveMeListOfBloxInTheUnderfieldPointsFor(x, y, list) {
+    const g = this.g;
+    list.length = 0;
+    const fieldlocx = Math.trunc((x - g.LEFT) / g.bloxDist);
+    const fieldlocy = Math.trunc((y - g.TOP) / g.bloxDist);
+    const fieldlocxOff = (x - g.LEFT) % g.bloxDist;
+    const fieldlocyOff = (y - g.TOP) % g.bloxDist;
+    if (!this._valid(fieldlocx, fieldlocy)) return;
+    this._addAll(list, this.underfield[fieldlocx][fieldlocy]);
+    if (fieldlocxOff > 0 && fieldlocx + 1 < g.BLoxCntX && this._valid(fieldlocx + 1, fieldlocy)) {
+      this._addAll(list, this.underfield[fieldlocx + 1][fieldlocy]);
+    }
+    if (fieldlocyOff > 0 && fieldlocy + 1 < g.BLoxCntY && this._valid(fieldlocx, fieldlocy + 1)) {
+      this._addAll(list, this.underfield[fieldlocx][fieldlocy + 1]);
+    }
+    if (fieldlocxOff > 0 && fieldlocx + 1 < g.BLoxCntX && fieldlocyOff > 0 && fieldlocy + 1 < g.BLoxCntY) {
+      this._addAll(list, this.underfield[fieldlocx + 1][fieldlocy + 1]);
+    }
+  }
+
+  _valid(x, y) {
+    return x >= 0 && y >= 0 && x < this.g.BLoxCntX && y < this.g.BLoxCntY;
+  }
+
+  _addAll(dst, src) {
+    for (let i = 0; i < src.length; i++) dst.push(src[i]);
+  }
+}
