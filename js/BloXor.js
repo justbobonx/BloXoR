5b81c2239361639beb329f165f9d4a0981cb6de7
just bob <rsmcclure75@gmail.com>
2026-09-01 12:30:45 -0400

    Restore BloXor.js: camera copies, delayed compact score with yellow bests

--- js/BloXor.js
+++ js/BloXor.js
@@ -1,1 +1,9 @@
-PLACEHOLDER
+const GameState = {
+  Title: 'Title',
+  InPlay: 'InPlay',
+  Paused: 'Paused',
+  LevelBeat: 'LevelBeat',
+  WaitToStartNewLevel: 'WaitToStartNewLevel',
+  GameOver: 'GameOver',
+  NoDraw: 'NoDraw'
+};
