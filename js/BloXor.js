27e3d1a469a5fbc927cb5417e3f11aa4027293d4
just bob <317064714+justbobonx@users.noreply.github.com>
2026-09-04 20:20:17 -0400

    Restore BloXor.js and show tilt wait screen after resume

--- js/BloXor.js
+++ js/BloXor.js
@@ -1,1 +1,9 @@
-placeholder
+const GameState = {
+  Title: 'Title',
+  InPlay: 'InPlay',
+  Paused: 'Paused',
+  LevelBeat: 'LevelBeat',
+  WaitToStartNewLevel: 'WaitToStartNewLevel',
+  GameOver: 'GameOver',
+  NoDraw: 'NoDraw'
+};
